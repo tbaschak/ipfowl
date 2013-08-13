f53f2273a6ea85347e06c4fbd856c52ff4736aba
Theo <theo@voinetworks.ca>
2013-08-12 19:10:41 -0500

    add variables for ipv4/ipv6 site name

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+// Various variables used
+$site="ip.henchman21.net";
+$4site="4." . $site;
+$6site="6." . $site;
+
+?>
--- htdocs/index.php
+++ htdocs/index.php
@@ -1,4 +1,11 @@
 <?php
+include "../config.php";
 $ip=$_SERVER['REMOTE_ADDR'];
 echo "Your IP address is: " . $ip;
+echo <<<EOF
+<hr width=95% noshade>
+<a href="$4site">IPv4 Only</a> - 
+<a href="$site">IPv4/IPv6</a> -
+<a href="$6site">IPv6 Only</a>
+EOF;
 ?>
