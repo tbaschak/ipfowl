45ba4816abff3d7ea14bdd475b724344e85c304c
Theo <theo@voinetworks.ca>
2013-08-12 19:12:23 -0500

    fix variable naming issue

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 // Various variables used
 $site="ip.henchman21.net";
-$4site="4." . $site;
-$6site="6." . $site;
+$site4="4." . $site;
+$site6="6." . $site;
 
 ?>
--- htdocs/index.php
+++ htdocs/index.php
@@ -4,8 +4,8 @@ $ip=$_SERVER['REMOTE_ADDR'];
 echo "Your IP address is: " . $ip;
 echo <<<EOF
 <hr width=95% noshade>
-<a href="$4site">IPv4 Only</a> - 
+<a href="$site4">IPv4 Only</a> - 
 <a href="$site">IPv4/IPv6</a> -
-<a href="$6site">IPv6 Only</a>
+<a href="$site6">IPv6 Only</a>
 EOF;
 ?>
