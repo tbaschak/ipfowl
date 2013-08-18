fa8abd3160ada1d1e1c53c79b584ff66ef4e8816
Theo Baschak <theo@voinetworks.ca>
2013-08-18 00:00:04 -0500

    add more html, and fork ribbon

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,7 +1,21 @@
-<table align=center>
- <tr>
-  <td><img src="images/chicken.png" align=left></td>
-  <td>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<html>
+<head>
+  <title>IP Fowl</title>
+</head>
+
+<body>
+  <a href="https://github.com/tbaschak/ipfowl"><img style=
+  "position: absolute; top: 0; right: 0; border: 0;" src=
+  "https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"
+  alt="Fork me on GitHub"></a>
+
+  <table align="center">
+    <tr>
+      <td><img src="images/chicken.png" align="left"></td>
+
+      <td>
 <?php
 include "../config.php";
 $ip=$_SERVER['REMOTE_ADDR'];
@@ -13,6 +27,9 @@ echo <<<EOF
 <a href="$proto$site6">IPv6 Only</a>
 EOF;
 ?>
-</td>
-</tr>
-</table>
+      </td>
+    </tr>
+  </table>
+</body>
+</html>
+
